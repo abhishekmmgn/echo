15c1ebac44453bd000a12de3c7f0bb21595bb71e
Abhishek Mamgain <abhishekmmgn@gmail.com>
2024-07-08 00:41:58 +0530

    Added analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,27 +6,31 @@ import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/lib/theme-provider";
 import { SocketProvider } from "./lib/socket-provider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 // Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Auth0Provider
-    domain={import.meta.env.VITE_AUTH0_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID!}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-    useRefreshTokens
-    cacheLocation="localstorage"
-  >
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <SocketProvider>
-          <App />
-        </SocketProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-    <Toaster />
-  </Auth0Provider>
+  <>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID!}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      useRefreshTokens
+      cacheLocation="localstorage"
+    >
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <SocketProvider>
+            <App />
+          </SocketProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+      <Toaster />
+    </Auth0Provider>
+    <Analytics />
+  </>
 );
